cd522f2eeccc32a7c87bb84a179f50015fb3146b
StefanDimeskiAsseco <Stefan.Dimeski@asseco-see.mk>
2023-04-12 15:22:25 +0200

    first

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _productsRepo;
+
+        public ProductsController(IGenericRepository<Product> productsRepo)
+        {
+            _productsRepo = productsRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+
+            var products = await _productsRepo.ListAsyncBySpecification(spec);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+            var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Core.Specifications;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsyncBySpecification(ISpecification<T> spec);
+    }
+}
